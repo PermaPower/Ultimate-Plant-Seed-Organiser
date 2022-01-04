bbb591972f4b8577117683713f02ecc1c725be93
David Power <dpower19@gmail.com>
2022-01-05 09:28:00 +1100

    Initial Commit

---
+++ Ultimate Plant & Seed Organiser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ultimate Plant & Seed Organiser
+//
+//  Created by David Power on 5/1/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
